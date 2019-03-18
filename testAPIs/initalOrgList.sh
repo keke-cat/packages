f1dba771e4f4c42773ba04a2c1899fe8e013285d
Roles_le <13051960278@163.com>
2019-03-18 14:01:27 +0800

    del testApis

--- testAPIs/initalOrgList.sh
+++
@@ -1,9 +0,0 @@
-portNum=7
-echo -n > ~/orgList
-MaxNum=${1:-20}
-
-for i in `seq 1 ${MaxNum}`;
-do
-  echo "org${i};peer0 ${portNum}051,${portNum}053;peer1 ${portNum}056,${portNum}058" >> ~/orgList
-  portNum=$(expr $portNum + 1)
-done
